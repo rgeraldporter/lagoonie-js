8da7e3fbd8ab0a7351edf6293551254696edcd0a
Rob Porter <rob@weeverapps.com>
2013-09-07 23:59:37 -0400

    Added CSV conversion for eBird Record format importation, corrected GCFL spelling and scientific name

--- lagoonie.js
+++ lagoonie.js
@@ -1,8 +1,116 @@
 
+/**
+* Converts a value to a string appropriate for entry into a CSV table.  E.g., a string value will be surrounded by quotes.
+* @param {string|number|object} theValue
+* @param {string} sDelimiter The string delimiter.  Defaults to a double quote (") if omitted.
+*/
+function toCsvValue(theValue, sDelimiter) {
+	var t = typeof (theValue), output;
+ 
+	if (typeof (sDelimiter) === "undefined" || sDelimiter === null) {
+		sDelimiter = '"';
+	}
+ 
+	if (t === "undefined" || t === null) {
+		output = "";
+	} else if (t === "string") {
+		output = sDelimiter + theValue + sDelimiter;
+	} else {
+		output = String(theValue);
+	}
+ 
+	return output;
+}
+ 
+/**
+* Converts an array of objects (with identical schemas) into a CSV table.
+* @param {Array} objArray An array of objects.  Each object in the array must have the same property list.
+* @param {string} sDelimiter The string delimiter.  Defaults to a double quote (") if omitted.
+* @param {string} cDelimiter The column delimiter.  Defaults to a comma (,) if omitted.
+* @return {string} The CSV equivalent of objArray.
+*/
+function toCsv(objArray, sDelimiter, cDelimiter) {
+	var i, l, names = [], name, value, obj, row, output = "", n, nl;
+ 
+	// Initialize default parameters.
+	if (typeof (sDelimiter) === "undefined" || sDelimiter === null) {
+		sDelimiter = '"';
+	}
+	if (typeof (cDelimiter) === "undefined" || cDelimiter === null) {
+		cDelimiter = ",";
+	}
+ 
+	for (i = 0, l = objArray.length; i < l; i += 1) {
+		// Get the names of the properties.
+		obj = objArray[i];
+		row = "";
+		if (i === 0) {
+			// Loop through the names
+			for (name in obj) {
+				if (obj.hasOwnProperty(name)) {
+					names.push(name);
+					row += [sDelimiter, name, sDelimiter, cDelimiter].join("");
+				}
+			}
+			row = row.substring(0, row.length - 1);
+			output += row;
+		}
+ 
+		output += "\n";
+		row = "";
+		for (n = 0, nl = names.length; n < nl; n += 1) {
+			name = names[n];
+			value = obj[name];
+			if (n > 0) {
+				row += ","
+			}
+			row += toCsvValue(value, '"');
+		}
+		output += row;
+	}
+ 
+	return output;
+}
+
+window.speciesReport = function( config ) {
+
+	for( var column in config ) {
+	
+		this[ column ] = config[ column ];
+	
+	}
+	
+}
+
+window.speciesReportCollection = function( speciesReports ) {
+
+	var elements = [];
+	
+	for( var report in speciesReports ) {
+	
+		elements.push( report );
+	
+	}
+	
+	this.add = function( speciesReport ) {
+	
+		elements.push( speciesReport );
+	
+	}
+	
+	this.getAll = function() {
+	
+		return elements;
+	
+	}
+
+}
+
 window.onload = function() {
 
 	var submitButton	= document.getElementById( "lagoonie-process" );
 	var resetButton		= document.getElementById( "lagoonie-reset" );
+	var speciesReports	= new window.speciesReportCollection();
 	
 	window.resetList	= function() {
 	
@@ -195,14 +303,48 @@ window.onload = function() {
 			
 			}
 			
+			var fullNotes = ( count.female ? count.female + "f " : "" ) + ( count.male ? count.male + "m " : "" ) + ( count.immature ? count.immature + "imm " : "" ) + ( count.juvenile ? count.juvenile + "juv" : "" ) + ( count.deceased ? count.deceased + "dec" : "" ) + count.notes + ( count.deceased ? count.deceased + " found deceased." : "" );
+			
 			result += "<tr><td>" + birdBrain[ bandCode ].name + "</td><td><b>" + ( count.total + count.male + count.female+count.juvenile + count.immature + count.deceased ) + "</b> "+ ( count.female ? count.female + "f " : "" ) + ( count.male ? count.male + "m " : "" ) + ( count.immature ? count.immature + "imm " : "" ) + ( count.juvenile ? count.juvenile + "juv" : "" ) + ( count.deceased ? count.deceased + "dec" : "" ) + "</td><td><em>"+ count.notes + ( count.deceased ? count.deceased + " found deceased." : "" ) +"</em></td></tr>";
+				
+			speciesReports.add( new window.speciesReport({
+			
+					"Common Name":					birdBrain[ bandCode ].name,
+					"Genus":						"",
+					"Species":						"",
+					"Number":						count.total,
+					"Species Comments":				fullNotes,
+					"Location Name":				"My test location",
+					"Latitude":						"",
+					"Longitude":					"",
+					"Date":							"09/07/2013",
+					"Start Time":					"13:30",
+					"State/Province":				"ON",
+					"Country Code":					"CA",
+					"Protocol":						"Traveling",
+					"Number of Observers":			1,
+					"Duration":						60,
+					"All observations reported?":	"Y",
+					"Effort Distance Miles":		1,
+					"Effort Area Acres":			"",
+					"Submission Comments":			"This is a test.."
+			
+				})
+				
+			);
 				
 		
 		}
 		
+		var csvResult = toCsv( speciesReports.getAll() ).split( "\n" ).slice( 1 ).join( "\n" );
+		
 		result += "</table>";
 		
+		result += "<textarea style='width: 25em; height: 15em;'>"+ csvResult +"</textarea>";
+		
 		resultHTML.innerHTML = result;
+		
+		speciesReports = new window.speciesReportCollection();
 	
 	}
 
