625295e87e0cd3edb5a6cf8bbaf10e80eb73b8f3
Rob Porter <rob@weeverapps.com>
2013-09-21 15:07:43 -0400

    Various fixes and minor refactor

--- lagoonie.js
+++ lagoonie.js
@@ -72,9 +72,74 @@ function toCsv(objArray, sDelimiter, cDelimiter) {
 	return output;
 }
 
-lagoonie = {};
+lagoonie 								= {};
+lagoonie.dom 							= {};
+lagoonie.singleton						= {};
+lagoonie.const							= {};
 
-lagoonie.speciesReport = function( config ) {
+( function(global) {
+	
+	"use strict";
+	
+	lagoonie.singleton.speciesReportsCollection = function() {
+		
+		var elements = [];
+
+		if ( lagoonie.singleton.speciesReportsCollection.prototype._singletonInstance )
+			return lagoonie.singleton.speciesReportsCollection.prototype._singletonInstance;
+		
+		lagoonie.singleton.speciesReportsCollection.prototype._singletonInstance = this;
+		
+		this.clear = function() {
+			
+			elements = [];
+			
+		}
+		
+		this.add = function( data ) {
+			
+			if( !(data instanceof Array) && !(data instanceof lagoonie.speciesReport) ) {
+			
+				console.log( "speciesReport not an array or instanceof lagoonie.speciesReport" );
+			
+				return false;
+				
+			}
+		
+			if( data instanceof Array ) {
+				
+				for( var report in data ) {
+				
+					elements.push( data[report] );
+				
+				}
+				
+			} else {
+				
+				elements.push( data );
+				
+			}
+
+		}
+		
+		this.getAll = function() {
+		
+			return elements;
+		
+		}
+
+		lagoonie.reports	= this;
+		
+	};
+
+	var a	= new lagoonie.singleton.speciesReportsCollection();
+	var b	= lagoonie.singleton.speciesReportsCollection();
+	
+	global.result = a === b;
+
+}(window) );
+
+lagoonie.speciesReport 	= function( config ) {
 
 	for( var column in config ) {
 	
@@ -84,44 +149,21 @@ lagoonie.speciesReport = function( config ) {
 	
 }
 
-lagoonie.speciesReportCollection = function( speciesReports ) {
-
-	var elements = [];
-	
-	for( var report in speciesReports ) {
-	
-		elements.push( report );
-	
-	}
-	
-	this.add = function( speciesReport ) {
-	
-		elements.push( speciesReport );
-	
-	}
-	
-	this.getAll = function() {
-	
-		return elements;
-	
-	}
-
-}
+// CONST
+lagoonie.const.START_LINE	= 2;
 
 window.onload = function() {
 
-	var submitButton	= document.getElementById( "lagoonie-process" );
-	var resetButton		= document.getElementById( "lagoonie-reset" );
-	var appendButton	= document.getElementById( "lagoonie-append" );
-	var speciesReports	= new lagoonie.speciesReportCollection();
+	lagoonie.dom.submitButton		= document.getElementById( "lagoonie-process" );
+	lagoonie.dom.resetButton		= document.getElementById( "lagoonie-reset" );
+	lagoonie.dom.appendButton		= document.getElementById( "lagoonie-append" );
+	lagoonie.dom.textArea			= document.getElementById( "lagoonie-textarea" );
+	lagoonie.dom.resultHTML			= document.getElementById( "lagoonie-result" );
 	
 	lagoonie.resetList	= function() {
-	
-		var textarea	= document.getElementById( "lagoonie-textarea" );
-			
-		// allow appendage
-		appendButton.style.display 	= "none";
-		textarea.value 				= "";
+
+		lagoonie.dom.appendButton.style.display 	= "none";
+		lagoonie.dom.textArea.value 				= "";
 	
 	}
 	
@@ -144,267 +186,289 @@ window.onload = function() {
 	
 	lagoonie.appendList		= function () {
 	
-		lagoonie.processList( true );
+		return new lagoonie.list( true );
 	
 	}
-
-	lagoonie.processList	= function( append ) {
 	
-		console.log( append );
+	lagoonie.speciesCount	= function() {
+		
+		this.total		= 0;
+		this.male		= 0;
+		this.female		= 0;
+		this.juvenile	= 0;
+		this.immature	= 0;
+		this.deceased	= 0;
+		this.notes		= "";
+				
+	}
 	
-		var textarea	= document.getElementById( "lagoonie-textarea" ),
-			listContent	= textarea.value,
-			lines		= listContent.split(/\n/),
-			result		= "",
-			resultHTML	= document.getElementById( "lagoonie-result" ),
-			metadata	= lagoonie.processMetadata( lines );
-			
-		// allow appendage
-		appendButton.style.display = "block";
+	lagoonie.processList	= function() {
+		
+		return new lagoonie.list();
+		
+	}
+		
+	lagoonie.list 			= function( append ) {
+		
+		!!! append && lagoonie.reports.clear();
+		
+		this.lines				= lagoonie.dom.textArea.value.split(/\n/);
+		this.result				= "";
+		this.metadata			= lagoonie.processMetadata( this.lines );
+		this.error				= false;
+		this.reportCollection	= [];
+		
+		lagoonie.dom.appendButton.style.display = "block";
 		
 		// if km is appended to distance, convert to miles
-		if( metadata.distance.search(/km/i) != -1 )
-			metadata.distance = parseFloat( metadata.distance.slice(0, -2) * 0.6214 ).toFixed(2);
-
-		result = "<p>REPORT FOR: "+ lines[0] +"<br />"+ (lines.length-1) +" species observed.</p>"+
-					"<table><tr><th>Species Name</th><th>Observed</th><th>Notes</th></tr>";
-					
-		for( var i = 2; i < lines.length; i++ ) {
-		
-			lines[i] = lines[i].trim();
-		
-			var chunks		= lines[i].split(' '),
-				bandCode 	= chunks[0],
-				count		= { total: 0, male: 0, female: 0, juvenile: 0, immature: 0, deceased: 0, notes: "" };				
+		if( this.metadata.distance.search(/km/i) != -1 || this.metadata.distance.search(/k/i) != -1 )
+			this.metadata.distance = parseFloat( this.metadata.distance.slice(0, -2) * 0.6214 ).toFixed(2);
 			
+		this.result += "<p>REPORT FOR: "+ this.lines[0] +"<br />"+ (this.lines.length-1) +" species observed.</p>"+
+		"<table><tr><th>Species Name</th><th>Observed</th><th>Notes</th></tr>";
+					
+		for( var lineNumber = lagoonie.const.START_LINE; lineNumber < this.lines.length; lineNumber++ ) {
+			
+			var currentLine 	= this.lines[lineNumber].trim(),
+				chunks			= currentLine.split(' '),
+				bandCode 		= chunks[0],
+				count			= new lagoonie.speciesCount();		
+				
+			if( bandCode == "" )
+				continue;		
+
 			if( !!! birdBrain[ bandCode ] ) {
 			
-				result += "<tr><td><b>Unreadable line:</b></td><td>" + lines[i] + "</td></tr>";
-				
+				this.result += "<tr><td><b style='color:red'>Unreadable line:</b></td><td style='background-color:#ffeeee;'>" + currentLine + " </td><td><b>[report will not be added to CSV until corrected]</b></td></tr>";
+				this.error	= true;
+
 				continue;
 			
 			}
 			
-			for( var ii = 1; ii < chunks.length; ii++ ) {
+			for( var chunkNumber = 1; chunkNumber < chunks.length; chunkNumber++ ) {
 			
-				var	prevNote	= !! isNote ? isNote : false,
-					isNote		= false;
+				var	previousWasNote		= !! isNote ? isNote : false,
+					isNote				= false,
+					chunkCount			= "";
 			
-				if( isNaN(chunks[ii]) ) {
-
-					var chunkCount = "";
+				if( !isNaN(chunks[chunkNumber]) ) {
 					
-					for( var char = 0; char < chunks[ii].length; char++ ) {
-					
-						// todo: account for 'mmfmfmffmj'-style notation
-					
-						var character 		= chunks[ii][char],
-							nextCharacter	= chunks[ii][char+1] ? chunks[ii][char+1] : null,
-							breakFor		= false,
-							addNotes		= function( singleCharacterChunk ) {
-						
-								if( !!singleCharacterChunk || (!!nextCharacter && isNaN(nextCharacter)) ) {
-									
-									count.notes 	+= ( count.notes && !prevNote ) ? "<br />" + chunks[ii].substring(char) + " " : chunks[ii].substring(char) + " ";
-									isNote			= true;
-									
-									return true;
-									
-								}
-								
-								return false;
-							
-							}
-						
-						if( ! isNaN(character) ) {
-						
-							chunkCount += character;
-						
-						} else {
-						
-							switch( character ) {
-							
-								case "m":
-								
-									if( addNotes() ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-								
-									if( chunkCount )
-										count.male += parseInt( chunkCount );
-									else 
-										count.male++;
-										
-									break;
-									
-								case "f":
-								
-									if( addNotes() ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-								
-									if( chunkCount )
-										count.female += parseInt( chunkCount );
-									else
-										count.female++;
-										
-									break;
-									
-								case "j":
-								
-									if( addNotes() ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-								
-									if( chunkCount )
-										count.juvenile += parseInt( chunkCount );
-									else
-										count.juvenile++;
-										
-									break;
-									
-								case "i":
-								
-									if( addNotes() ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-								
-									if( chunkCount )
-										count.immature += parseInt( chunkCount );
-									else
-										count.immature++;
-										
-									break;
-									
-								case "d":
-								
-									// append as note for eBird as well
-									if( addNotes() ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-								
-									if( chunkCount )
-										count.deceased += parseInt( chunkCount );
-									else
-										count.deceased++;
-										
-									break;
-									
-								default:
-								
-									if( addNotes(true) ) {
-									
-										breakFor = true;
-										
-										break;
-									
-									}
-									
-									break;
-
-							}
-							
-							chunkCount = "";
-						
-						}
-						
-						if( !!breakFor )
-							break;
-					
-					}
-				
+					count.total += parseInt( chunks[chunkNumber] );
 					continue;
 					
 				}
+
+				var chunkCount = "";
 				
-				count.total += parseInt( chunks[ii] );
-			
+				for( var characterNumber = 0; characterNumber < chunks[chunkNumber].length; characterNumber++ ) {
+				
+					// todo: account for 'mmfmfmffmj'-style notation
+				
+					var currentCharacter 		= chunks[chunkNumber][characterNumber],
+						nextCharacter			= chunks[chunkNumber][characterNumber+1] ? chunks[chunkNumber][characterNumber+1] : null,
+						breakFor				= false,
+						addNotes				= function( singleCharacterChunk ) {
+					
+							if( !!singleCharacterChunk || (!!nextCharacter && isNaN(nextCharacter)) ) {
+								
+								count.notes 	+= ( count.notes && !previousWasNote ) ? "<br />" + chunks[chunkNumber].substring(characterNumber) + " " : chunks[chunkNumber].substring(characterNumber) + " ";
+								isNote			= true;
+								
+								return true;
+								
+							}
+							
+							return false;
+						
+						}
+
+					if( ! isNaN(currentCharacter) ) {
+					
+						chunkCount += currentCharacter;
+					
+					} else {
+					
+						switch( currentCharacter ) {
+						
+							case "m":
+							
+								if( addNotes() ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+							
+								if( chunkCount )
+									count.male += parseInt( chunkCount );
+								else 
+									count.male++;
+									
+								break;
+								
+							case "f":
+							
+								if( addNotes() ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+							
+								if( chunkCount )
+									count.female += parseInt( chunkCount );
+								else
+									count.female++;
+									
+								break;
+								
+							case "j":
+							
+								if( addNotes() ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+							
+								if( chunkCount )
+									count.juvenile += parseInt( chunkCount );
+								else
+									count.juvenile++;
+									
+								break;
+								
+							case "i":
+							
+								if( addNotes() ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+							
+								if( chunkCount )
+									count.immature += parseInt( chunkCount );
+								else
+									count.immature++;
+									
+								break;
+								
+							case "d":
+							
+								// append as note for eBird as well
+								if( addNotes() ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+							
+								if( chunkCount )
+									count.deceased += parseInt( chunkCount );
+								else
+									count.deceased++;
+									
+								break;
+								
+							default:
+							
+								if( addNotes(true) ) {
+								
+									breakFor = true;
+									
+									break;
+								
+								}
+								
+								break;
+					
+						}
+						
+						chunkCount = "";
+					
+					}
+					
+					
+					if( !!breakFor )
+						break;
+				
+				}
+				
 			}
 			
 			var fullNotes 	= ( count.female ? count.female + "f " : "" ) + ( count.male ? count.male + "m " : "" ) + ( count.immature ? count.immature + "imm " : "" ) + ( count.juvenile ? count.juvenile + "juv" : "" ) + ( count.deceased ? count.deceased + "dec" : "" ) + count.notes + ( count.deceased ? count.deceased + " found deceased." : "" ),
 				fullCount	= ( count.total + count.male + count.female + count.juvenile + count.immature + count.deceased );
 			
-			result += "<tr><td>" + birdBrain[ bandCode ].name + "</td><td><b>" + ( count.total + count.male + count.female+count.juvenile + count.immature + count.deceased ) + "</b> "+ ( count.female ? count.female + "f " : "" ) + ( count.male ? count.male + "m " : "" ) + ( count.immature ? count.immature + "imm " : "" ) + ( count.juvenile ? count.juvenile + "juv" : "" ) + ( count.deceased ? count.deceased + "dec" : "" ) + "</td><td><em>"+ count.notes + ( count.deceased ? count.deceased + " found deceased." : "" ) +"</em></td></tr>";
+			this.result += "<tr><td>" + birdBrain[ bandCode ].name + "</td><td><b>" + ( count.total + count.male + count.female+count.juvenile + count.immature + count.deceased ) + "</b> "+ ( count.female ? count.female + "f " : "" ) + ( count.male ? count.male + "m " : "" ) + ( count.immature ? count.immature + "imm " : "" ) + ( count.juvenile ? count.juvenile + "juv" : "" ) + ( count.deceased ? count.deceased + "dec" : "" ) + "</td><td><em>"+ count.notes + ( count.deceased ? count.deceased + " found deceased." : "" ) +"</em></td></tr>";
 			
 			var date = new Date();
 			
 			// assume protocol based on metadata given
-			if( !! metadata.distance && !! metadata.duration )
-				metadata.protocol	= "Traveling";
-			else if( !! metadata.duration && ! metadata.distance )
-				metadata.protocol	= "Stationary";
+			if( !! this.metadata.distance && !! this.metadata.duration )
+				this.metadata.protocol	= "Traveling";
+			else if( !! this.metadata.duration && ! this.metadata.distance )
+				this.metadata.protocol	= "Stationary";
 			else 
-				metadata.protocol	= "Casual";
+				this.metadata.protocol	= "Casual";
 
 			// adding year, assuming current year
-			if( metadata.date.length == 5 )
-				metadata.date += "-" + date.getFullYear();
+			if( this.metadata.date.length == 5 )
+				this.metadata.date += "-" + date.getFullYear();
 				
-			metadata.date = metadata.date.replace( /-/g, "/" );
+			this.metadata.date = this.metadata.date.replace( /-/g, "/" );
+			
+			console.log( this.error );
+			
+			var birdReport = new lagoonie.speciesReport({
+			
+				"Common Name":					birdBrain[ bandCode ].name,
+				"Genus":						"",
+				"Species":						"",
+				"Number":						fullCount,
+				"Species Comments":				fullNotes,
+				"Location Name":				this.metadata.location,
+				"Latitude":						"",
+				"Longitude":					"",
+				"Date":							this.metadata.date,
+				"Start Time":					this.metadata.time,
+				"State/Province":				"ON",
+				"Country Code":					"CA",
+				"Protocol":						this.metadata.protocol,
+				"Number of Observers":			1,
+				"Duration":						this.metadata.duration,
+				"All observations reported?":	"Y",
+				"Effort Distance Miles":		this.metadata.distance,
+				"Effort Area Acres":			"",
+				"Submission Comments":			"List generated using Lagoonie v0.5, written by Rob Porter."
+			
+			});
 								
-			speciesReports.add( new lagoonie.speciesReport({
+			this.reportCollection.push( birdReport );
 			
-					"Common Name":					birdBrain[ bandCode ].name,
-					"Genus":						"",
-					"Species":						"",
-					"Number":						fullCount,
-					"Species Comments":				fullNotes,
-					"Location Name":				metadata.location,
-					"Latitude":						"",
-					"Longitude":					"",
-					"Date":							metadata.date,
-					"Start Time":					metadata.time,
-					"State/Province":				"ON",
-					"Country Code":					"CA",
-					"Protocol":						metadata.protocol,
-					"Number of Observers":			1,
-					"Duration":						metadata.duration,
-					"All observations reported?":	"Y",
-					"Effort Distance Miles":		metadata.distance,
-					"Effort Area Acres":			"",
-					"Submission Comments":			"List generated using Lagoonie, written by Rob Porter."
-			
-				})
-				
-			);
+			console.log( this.reportCollection)
 				
 		
 		}
 		
-		var csvResult = toCsv( speciesReports.getAll() ).split( "\n" ).slice( 1 ).join( "\n" );
+		this.error || lagoonie.reports.add( this.reportCollection );
 		
-		result += "</table>";
+		var csvResult = toCsv( lagoonie.reports.getAll() ).split( "\n" ).slice( 1 ).join( "\n" );
 		
-		result += "<textarea style='width: 25em; height: 15em;'>"+ csvResult +"</textarea>";
+		this.result += "</table>";
 		
-		resultHTML.innerHTML = result;
+		this.result += "<textarea style='width: 25em; height: 15em;'>"+ csvResult +"</textarea>";
 		
-		if( true !== append ) {
+		lagoonie.dom.resultHTML.innerHTML = this.result;
+		
+		console.log( this.result );
 
-			speciesReports = new lagoonie.speciesReportCollection();
-			
-		}
-		
 		encodedUri = encodeURI( "data:text/csv;charset=utf-8," + csvResult );
 		
 		var link = document.createElement("a");
@@ -413,19 +477,19 @@ window.onload = function() {
 		link.setAttribute("download", "ebird-ready.csv");
 		link.innerHTML = "DOWNLOAD";
 		
-		resultHTML.appendChild( link );
+		lagoonie.dom.resultHTML.appendChild( link );
 		
 		//link.click(); // This will download the data file named "my_data.csv".
 	
 	}
 
-	submitButton.addEventListener( 'touchend', lagoonie.processList );
-	submitButton.addEventListener( 'click', lagoonie.processList );
+	lagoonie.dom.submitButton.addEventListener( 'touchend', lagoonie.processList );
+	lagoonie.dom.submitButton.addEventListener( 'click', lagoonie.processList );
 	
-	resetButton.addEventListener( 'touchend', lagoonie.resetList );
-	resetButton.addEventListener( 'click', lagoonie.resetList );
+	lagoonie.dom.resetButton.addEventListener( 'touchend', lagoonie.resetList );
+	lagoonie.dom.resetButton.addEventListener( 'click', lagoonie.resetList );
 	
-	appendButton.addEventListener( 'touchend', lagoonie.appendList );
-	appendButton.addEventListener( 'click', lagoonie.appendList );
+	lagoonie.dom.appendButton.addEventListener( 'touchend', lagoonie.appendList );
+	lagoonie.dom.appendButton.addEventListener( 'click', lagoonie.appendList );
 
 }
